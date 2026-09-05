30dc613f055b84d3dbef320ec5d16cbc098668fc
gtpesanan-ux <apiraja8888@gmail.com>
2026-09-05 16:43:20 +0700

    Add files via upload

---
+++ assets/portal-config.js
@@ -0,0 +1,6 @@
+/* Pengaturan alamat portal pelanggan.
+ * Kosongkan origin agar /masuk.html mengikuti domain/subdomain website aktif.
+ * Jika portal berada di host terpisah, isi HANYA origin HTTPS portal milik Anda.
+ * Tidak perlu mengedit 38 HTML. Jangan isi password, token, atau data rahasia.
+ */
+window.DIGDAYA_PORTAL_CONFIG = Object.freeze({ origin: "" });
